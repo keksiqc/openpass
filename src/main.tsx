8b000072a9209fc8eaecbc13dbd72e4e8e861519
Keksi <me@keksi.dev>
2025-06-03 08:23:16 +0000

    fix: add biome-ignore comment for non-null assertion in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
+// biome-ignore lint/style/noNonNullAssertion: We are sure that 'root' exists in the HTML.
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
