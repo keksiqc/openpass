6fd5715203eec3f2cb1290bc4863b63ddac5e82c
marlonhotz07 <marlonhotz07@gmail.com>
2025-07-07 07:34:36 +0000

    chore: add dev environment config with Nix and EditorConfig setup

---
+++ .idx/dev.nix
@@ -0,0 +1,19 @@
+{pkgs}: {
+  channel = "stable-25.05";
+  packages = [
+    pkgs.nodejs_24
+    pkgs.pnpm
+  ];
+  idx.extensions = [
+    "biomejs.biome"
+    "EditorConfig.EditorConfig"
+    "usernamehw.errorlens"
+    "tamasfe.even-better-toml"
+    "YoavBls.pretty-ts-errors"
+    "Codeium.codeium"
+    "castrogusttavo.symbols"
+    "antfu.icons-carbon"
+    "antfu.file-nesting"
+    "bradlc.vscode-tailwindcss"
+  ];
+}
