0cd6e1a4d0823c11dc8fc1ee326e2d859393bdfd
Keksi <git@keksi.dev>
2026-05-03 10:30:58 +0200

    refactor: migrate to Vite Plus and update configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vite-plus";
 
 // https://vite.dev/config/
 export default defineConfig({
+  staged: {
+    "*": "vp check --fix",
+  },
+  fmt: {},
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
